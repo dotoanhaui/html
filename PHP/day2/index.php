afcd2a6e44af277c4b47be7f2053101989c55f10
ADMIN <dotoan.haui@gmail.com>
2019-05-22 20:46:18 +0700

    html

---
+++ PHP/day2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1> câu lệnh điều kiện trong php</h1>
+    <?php
+    $age = 19;
+    if($age <18)
+        echo "<br>trẻ em";
+    else
+        echo "<br>người trưởn thành";
+    ?>
+</body>
+</html>
